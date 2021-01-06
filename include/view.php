aca6e97ed5ca8e730bf75601ae98d303ff138926
veith.buergerhoff <veith.buergerhoff@gmail.com>
2021-01-06 23:12:03 +0100

    Sehr gut beschriebenen Bug behoben. ❤

--- include/view.php
+++ include/view.php
@@ -36,7 +36,26 @@
             <script>
                 window.addEventListener('load', fahrten("<?php echo $year; ?>", "<?php echo $month; ?>"));
             </script>
-            <div id="table_view"></div>
+            <div>
+
+            <div class="modal fade" id="preview" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" id="modal_fahrt_preview">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Schließen</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="table_view"></div>
+            </div>
             <?php
         }
     }
--- require/getTable.php
+++ require/getTable.php
@@ -4,23 +4,6 @@ $month = $_GET["month"];
 $year = $_GET["year"];
 $sql = mysqli_query( $con, "SELECT * FROM fahrten WHERE year(datum) = '$year' AND month(datum) = '$month'" );
 ?>
-<div class="modal fade" id="preview" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" id="modal_fahrt_preview">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Schließen</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <table class="table table-hover table-responsive">
     <thead>
         <tr>
